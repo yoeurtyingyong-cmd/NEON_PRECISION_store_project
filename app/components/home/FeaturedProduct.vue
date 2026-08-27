6caef7ae0fa58dfbfc39f333311132a4678be27d
picheng5555kh-arch <picheng5555kh@gmail.com>
2026-08-27 16:49:18 +0700

    categevb

---
+++ app/components/home/FeaturedProduct.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div>
+            <p>
+            </p>
+            <button>
+
+            </button>
+        </div>
+        <div>
+            <div>
+               <div>
+                <p></p>
+                <p></p>
+               </div>
+               <img src="" alt="">
+            </div>
+            <div>
+                <div>
+                <img src="" alt="">
+                <div>
+                    <p></p>
+                    <p></p>
+                    <img src="" alt="">
+                </div>
+                <div>
+                <img src="" alt="">
+                <div>
+                    <p></p>
+                    <p></p>
+                    <img src="" alt="">
+                </div>
+            </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ app/components/home/HeroBanner.vue
@@ -0,0 +1,14 @@
+<template>
+    <select name="" id="">
+        <img src="" alt="">
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <div>
+                <Button></Button>
+                <Button></Button>
+            </div>
+        </div>
+    </select>
+</template>
